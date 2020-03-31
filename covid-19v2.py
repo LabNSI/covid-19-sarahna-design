e7a6aa6d1dc48dd3fdfdc6d7410a4551ae6f9ea8
sarahna-design <59612074+sarahna-design@users.noreply.github.com>
2020-03-31 16:42:20 +0200

    Update covid-19v2.py

--- covid-19v2.py
+++ covid-19v2.py
@@ -17,17 +17,17 @@ def download_file(url, file_name):
     '''
     try:
         # open url passed in argument
-        file = urllib2.urlopen(___)
+        file = urllib2.urlopen(url)
         # open file for writing in binary mode
         # https://docs.python.org/3/library/functions.html#open
-        with open(___, '___') as output:
+        with open(file_name, 'wb') as output:
             # write in output the file read
-            output.___(file.___())
+            output.write(file.read())
         # OK : return true
-        return ___
+        return True
     except:
         # Something is wrong : return false
-        return ___
+        return False
 
 def read_CSV(file):
     '''
@@ -41,11 +41,11 @@ def read_CSV(file):
     # https://docs.python.org/3/library/csv.html#csv.reader
     with open(file) as csvfile:
         # Create a dictionnary with all datas
-        reader = csv.DictReader(____)
+        reader = csv.DictReader(csvfile)
         # each row in reader is a dictionary by country
         for row in reader:
             # add row in datas list
-            datas.___(___)
+            datas.append(row)
         # return the list of dictionaries by countries
         return datas
 
@@ -54,9 +54,9 @@ def read_CSV(file):
 if __name__ == '__main__':
 
     # File name of datas
-    file = "time_series_19-covid-Confirmed.csv"
+    file = "time_series_19-covid-confirmed-global.csv"
     # where datas are located
-    url = "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+    url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 
     if download_file(url,file):
         print(f'Téléchargement du fichier {file} terminé avec succès')
@@ -67,7 +67,6 @@ if __name__ == '__main__':
             print(row['Province/State'], row['Country/Region'])
     else:
         print(f'Téléchargement du fichier {file} impossible')
-
     
     
 
