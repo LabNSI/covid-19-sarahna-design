2fe4846d946050f92e71bcdf1dbf4e7cde41c86d
sarahna-design <59612074+sarahna-design@users.noreply.github.com>
2020-03-31 16:43:12 +0200

    Update covid-19v3.py

--- covid-19v3.py
+++ covid-19v3.py
@@ -18,17 +18,17 @@ def download_file(url, file_name):
     '''
     try:
         # open url passed in argument
-        file = urllib2.urlopen(___)
+        file = urllib2.urlopen(url)
         # open file for writing in binary mode
         # https://docs.python.org/3/library/functions.html#open
-        with open(___, '___') as output:
+        with open(file_name, 'wb') as output:
             # write in output the file read
-            output.___(file.___())
+            output.write(file.read())
         # OK : return true
-        return ___
+        return True
     except:
         # Something is wrong : return false
-        return ___
+        return False
 
 
 def read_CSV(file):
@@ -37,26 +37,26 @@ def read_CSV(file):
     - fields name as string in a list
     - datas dictionary  by countries in a list
     '''
-    # datas is an empty list
+    # datas is an empty list 
     datas = []
     # open the file passed in argument as csvfile
     # https://docs.python.org/3/library/csv.html#csv.reader
     with open(file) as csvfile:
         # Create a dictionnary with all datas
-        reader = csv.DictReader(____)
+        reader = csv.DictReader(csvfile)
         # each row in reader is a dictionary by country
         for row in reader:
             # add row in datas list
-            datas.___(___)
+            datas.append(row)
         # return the list of dictionaries by countries
         return datas
 
 def data_for_country(data, state = "", country = ""):
     # for each country in data
-    for pays in ___:
+    for pays in data:
         # if 'Province\State' key match with state given in argument
         # and if 'Country/Region' key match with country given in argument
-        if pays[___] == ___ and pays[___] == ___:
+        if pays['Province/State'] == state and pays['Country/Region'] == country:
             return pays
 
 
@@ -64,9 +64,9 @@ def data_for_country(data, state = "", country = ""):
 if __name__ == '__main__':
 
   # File name of datas
-  file = "time_series_19-covid-Confirmed.csv"
+  file = "time_series_19-covid-confirmed-global.csv"
   # where datas are located
-  url = "https://github.com/CSSEGISandData/COVID-19/raw/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+  url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 
   if download_file(url,file):
       print(f'Téléchargement du fichier {file} terminé avec succès')
@@ -75,7 +75,7 @@ if __name__ == '__main__':
       print("----------------------------------")
       # for row in coutries:
       #    print(row['Province/State'], row['Country/Region'])
-      france = data_for_country(countries, 'France', 'France')
+      france = data_for_country(countries, '', 'France')
       print(france)
 
   else:
